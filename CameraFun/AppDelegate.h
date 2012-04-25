4f3ff173bb86ba49f90d3913fae781e8d71789a4
Hoa Chen <hchen1014@Hoa-Chens-MacBook-Pro.local>
2012-04-25 16:31:07 -0700

    Initial Commit

---
+++ CameraFun/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CameraFun
+//
+//  Created by Hoa Chen on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CameraFun/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  CameraFun
+//
+//  Created by Hoa Chen on 4/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
